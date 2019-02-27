d3cc5421b667af0c9bfa70b97be3690f5c6af634
Haneen Mohammed <hamohammed.sa@gmail.com>
2019-02-27 16:50:03 +0000

    add debug and test_connection script

--- onesession.py
+++ onesession.py
@@ -5,12 +5,16 @@ at the server side:
 def connect()
     return Response(json.dumps(["success"]))
 """
-import time
-import requests
+import logging, requests, time
+logging.basicConfig(level=logging.DEBUG, format="%(message)s")
+requests_log = logging.getLogger("requests.packages.urllib3")
+requests_log.setLevel(logging.DEBUG)
+requests_log.propagate = True
+
 
 timestep = lambda: int(round(time.time() * 1000))
-#url = "http://100.64.0.1:8000/test/connection"
-url = "http://0.0.0.0:5000/test/connection"
+url = "http://100.64.0.1:8000/test/connection"
+#url = "http://0.0.0.0:8000/test/connection"
 session = requests.Session()
 
 request_time = timestep()
--- server.py
+++ server.py
@@ -19,4 +19,4 @@ if __name__ == "__main__":
 
 
     setup_routes(app)
-    app.run(host="0.0.0.0", port=5000, debug=False, threaded=True)
+    app.run(host="0.0.0.0", port=8000, debug=False, threaded=True)
